9b888e6860b460395129bccb1de5a200ef281fc9
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-05-23 10:34:08 -0700

    update styling

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ function getMovies(searchText) {
                     <div class="col-md-3">
                         <div class="well text-center">
                             <img src="${movie.Poster}">
-                            <h5>${movie.Title}</h5>
+                            <h5 class="title">${movie.Title}</h5>
                             <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
                         </div>
                     </div>
